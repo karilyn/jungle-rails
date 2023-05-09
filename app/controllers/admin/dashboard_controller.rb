9f9bd8e4ea15d8dfeb56cfe287da4234fe7737fd
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-09 10:55:39 -0700

    Add prod + cat count instance variables

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,10 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
+
 
   protected
 
