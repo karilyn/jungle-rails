29b7562c6a7152f03f8cf8645f306e45d6f51fdd
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-12 13:29:35 -0700

    Add login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,12 +20,24 @@ Rails.application.routes.draw do
   end
 
   # these routes are for showing users a login form, logging them in, and logging them out.
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  # get '/login' => 'sessions#new'
+  # post '/login' => 'sessions#create'
+  # get '/logout' => 'sessions#destroy'
 
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  # sign up page with form:
+	get '/users/new' => 'users#new', as: :new_user
+
+	# create (post) action for when sign up form is submitted:
+	post '/users' => 'users#create'
+
+  # log in page with form:
+	get '/login'     => 'sessions#new'
+
+	# create (post) action for when log in form is submitted:
+	post '/login'    => 'sessions#create'
+
+	# delete action to log out:
+	delete '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
