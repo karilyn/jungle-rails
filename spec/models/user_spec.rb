85cecf08a6f8595302d3a1ec9d8e0bceb498db24
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-22 22:09:02 -0700

    Adjust validations to check first/last name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,17 @@ RSpec.describe User, type: :model do
   # validation tests/examples here
   describe 'Validations' do
     it "saves successfully when all fields are valid" do
-      @user = User.create(name: "Test Test", email: "test@test.com", password: "testing", password_confirmation: "testing")
+      @user = User.create(first_name: "Test", last_name: "Test", email: "test@test.com", password: "testing", password_confirmation: "testing")
       expect(@user).to be_valid
     end
 
-    it "fails to save when name is not set" do
-      @user = User.create(name: nil, email: "test@test.com", password: "testing", password_confirmation: "testing")
+    it "fails to save when first name is not set" do
+      @user = User.create(first_name: nil, last_name: "Test", email: "test@test.com", password: "testing", password_confirmation: "testing")
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "fails to save when last name is not set" do
+      @user = User.create(first_name: "Test", last_name: nil, email: "test@test.com", password: "testing", password_confirmation: "testing")
       expect(@user.errors.full_messages).to include("Name can't be blank")
     end
 
@@ -32,7 +37,15 @@ RSpec.describe User, type: :model do
       @user = User.create(name: "Test Test", email: "test@test.com", password: "testing", password_confirmation: "testing1")
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
-
   end
 
+  describe '.authenticate_with_credentials' do
+    it "authenticates when credentials are valid" do
+      auth = User.authenticate_with_credentials(email: "test@test.com", password: "testing")
+      expect(auth).to be_valid
+    end
+
+
+
+  end
 end
