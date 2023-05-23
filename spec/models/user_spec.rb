6b4fd575ba7372722f7e6db1bb8b13d27aaecbec
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-22 21:23:23 -0700

    Add tests for successful validation and name: nil

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # validation tests/examples here
-  it "saves successfully when all fields are set" do
-    @user = User.create(first_name: "Test", last_name: "Test", email: "test@test.com", password: "test", password_confirmation: "test")
-    expect(@user).to be_valid
+  describe 'Validations' do
+    it "saves successfully when all fields are valid" do
+      @user = User.create(name: "Test Test", email: "test@test.com", password: "testing", password_confirmation: "testing")
+      expect(@user).to be_valid
+    end
+
+    it "fails to save when name is not set" do
+      @user = User.create(name: nil, email: "test@test.com", password: "testing", password_confirmation: "testing")
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+    end
   end
+
 end
