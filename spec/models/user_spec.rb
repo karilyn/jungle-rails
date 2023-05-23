45ffe68b463637b96c8c3b8b245ff1f052e98cfc
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-22 22:55:48 -0700

    Format spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,65 +3,140 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # validation tests/examples here
   describe 'Validations' do
-    it "saves successfully when all fields are valid" do
-      @user = User.create(first_name: "Test", last_name: "Test", email: "test@test.com", password: "testing", password_confirmation: "testing")
-      expect(@user).to be_valid
+    it "saves user successfully when all fields are valid" do
+      user = User.create(
+        first_name: "Test",
+        last_name: "Test",
+        email: "test@test.com",
+        password: "testing",
+        password_confirmation: "testing"
+      )
+      expect(user).to be_valid
     end
 
     it "fails to save when first name is not set" do
-      @user = User.create(first_name: nil, last_name: "Test", email: "test@test.com", password: "testing", password_confirmation: "testing")
-      expect(@user.errors.full_messages).to include("Name can't be blank")
+      user = User.create(
+        first_name: nil,
+        last_name: "Test",
+        email: "test@test.com",
+        password: "testing",
+        password_confirmation: "testing"
+      )
+      expect(user.errors.full_messages).to include("Name can't be blank")
     end
 
     it "fails to save when last name is not set" do
-      @user = User.create(first_name: "Test", last_name: nil, email: "test@test.com", password: "testing", password_confirmation: "testing")
-      expect(@user.errors.full_messages).to include("Name can't be blank")
+      user = User.create(
+        first_name: "Test",
+        last_name: nil,
+        email: "test@test.com",
+        password: "testing",
+        password_confirmation: "testing"
+      )
+      expect(user.errors.full_messages).to include("Name can't be blank")
     end
 
     it "fails to save when email is not set" do
-      @user = User.create(name: "Test Test", email: nil, password: "testing", password_confirmation: "testing")
-      expect(@user.errors.full_messages).to include("Email can't be blank")
+      user = User.create(
+        first_name: "Test",
+        last_name: "Test",
+        email: nil,
+        password: "testing",
+        password_confirmation: "testing"
+      )
+      expect(user.errors.full_messages).to include("Email can't be blank")
     end
 
     it "fails to save when password is not set" do
-      @user = User.create(name: "Test Test", email: "test@test.com", password: nil, password_confirmation: "testing")
-      expect(@user.errors.full_messages).to include("Password can't be blank")
+      user = User.create(
+        first_name: "Test",
+        last_name: "Test",
+        email: "test@test.com",
+        password: nil,
+        password_confirmation: "testing"
+      )
+      expect(user.errors.full_messages).to include("Password can't be blank")
     end
 
     it "fails to save when password_confirmation is not set" do
-      @user = User.create(name: "Test Test", email: "test@test.com", password: "testing", password_confirmation: nil)
-      expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+      user = User.create(
+        first_name: "Test",
+        last_name: "Test",
+        email: "test@test.com",
+        password: "testing",
+        password_confirmation: nil
+      )
+      expect(user.errors.full_messages).to include("Password confirmation can't be blank")
     end
 
     it "fails to save when password and password_confirmation do not match" do
-      @user = User.create(name: "Test Test", email: "test@test.com", password: "testing", password_confirmation: "testing1")
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      user = User.create(
+        first_name: "Test",
+        last_name: "Test",
+        email: "test@test.com",
+        password: "testing",
+        password_confirmation: "testing1"
+      )
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match")
+    end
+
+    it "fails to save when password is shorter than six characters" do
+      user = User.create(
+        first_name: "Test",
+        last_name: "Test",
+        email: "test@test.com",
+        password: "test",
+        password_confirmation: "test"
+      )
+      expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters")
     end
   end
 
   describe '.authenticate_with_credentials' do
     it "authenticates when credentials are valid" do
-      auth = User.authenticate_with_credentials(email: "test@test.com", password: "testing")
+      user = User.create(
+        first_name: "Test",
+        last_name: "Test",
+        email: "test@test.com",
+        password: "testing",
+        password_confirmation: "testing"
+      )
+      auth = User.authenticate_with_credentials(
+        email: "test@test.com",
+        password: "testing"
+      )
       expect(auth).to be_valid
     end
 
     it "fails to authenticate when email is not valid" do
-      auth = User.authenticate_with_credentials(email: nil, password: "testing")
+      auth = User.authenticate_with_credentials(
+        email: nil,
+        password: "testing"
+      )
       expect(auth).to be_nil
     end
 
     it "fails to authenticate when password is not valid" do
-      auth = User.authenticate_with_credentials(email: "test@test.com", password: nil)
+      auth = User.authenticate_with_credentials(
+        email: "test@test.com",
+        password: nil
+      )
       expect(auth).to be_nil
     end
 
     it "authenticates when email has leading or trailing spaces" do
-      auth = User.authenticate_with_credentials(email: " test@test.com ", password: "testing")
+      auth = User.authenticate_with_credentials(
+        email: " test@test.com ",
+        password: "testing"
+      )
       expect(auth).to be_valid
     end
 
     it "authenticates when email is in the wrong case" do
-      auth = User.authenticate_with_credentials(email: "TEST@test.com", password: "testing")
+      auth = User.authenticate_with_credentials(
+        email: "TEST@test.com",
+        password: "testing"
+      )
       expect(auth).to be_valid
     end
 
