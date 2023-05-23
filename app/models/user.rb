b24f08d100ad8656036b0b6815dd5e90a48f6f5e
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-22 22:08:36 -0700

    Add first and last name to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
   validates_format_of :email, with: EMAIL_REGEX
 
   validates :email, presence: true, uniqueness: true
-  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
@@ -19,4 +20,5 @@ class User < ApplicationRecord
       nil
     end
   end
+
 end
