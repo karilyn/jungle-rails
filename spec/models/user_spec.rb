4e239550457ff2c9101463359ac2550eca596ddc
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-22 21:32:39 -0700

    Add tests validating email, pw, pw conf and pw matching

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,27 @@ RSpec.describe User, type: :model do
       @user = User.create(name: nil, email: "test@test.com", password: "testing", password_confirmation: "testing")
       expect(@user.errors.full_messages).to include("Name can't be blank")
     end
+
+    it "fails to save when email is not set" do
+      @user = User.create(name: "Test Test", email: nil, password: "testing", password_confirmation: "testing")
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "fails to save when password is not set" do
+      @user = User.create(name: "Test Test", email: "test@test.com", password: nil, password_confirmation: "testing")
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it "fails to save when password_confirmation is not set" do
+      @user = User.create(name: "Test Test", email: "test@test.com", password: "testing", password_confirmation: nil)
+      expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+    end
+
+    it "fails to save when password and password_confirmation do not match" do
+      @user = User.create(name: "Test Test", email: "test@test.com", password: "testing", password_confirmation: "testing1")
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
   end
 
 end
