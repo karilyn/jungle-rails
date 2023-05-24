88ffd2bd2f7d63f2af056cf5dea01524d16e4513
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-23 21:38:45 -0700

    Add first tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe('jungle home', () => {
+  it("should load the home page", () => {
+    cy.visit('/')
+  })
+
+  it("should show products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("should show two products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+})
